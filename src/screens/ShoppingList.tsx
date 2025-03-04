29b784a49a4f6619edcf509d7f087004598045e5
ThornThavann <thavann.thorn@institute.pse.ngo>
2025-03-04 13:22:29 +0700

    state

--- src/screens/ShoppingList.tsx
+++ src/screens/ShoppingList.tsx
@@ -1,29 +1,13 @@
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector} from "react-redux";
 import { RootState } from "../store/store";
-import { useEffect, useState } from "react";
+import { TodoList } from "./TodoList";
 
-  
-  interface Todo {
-    id: number;
-    title: string;
-    completed: boolean;
-  }
-  
-  export const ShoppingList = () => {
-    const navigate = useNavigate();
-    const items = useSelector((state: RootState) => state.shopping.items);
-    const userName = useSelector((state: RootState) => state.user.name);
-    
-    const [todos, setTodos] = useState<Todo[]>([]);
-  
-    useEffect(() => {
-      fetch("https://jsonplaceholder.typicode.com/todos")
-        .then((response) => response.json())
-        .then((data) => setTodos(data.slice())) // Limit to 10 items
-        .catch((error) => console.error("Error fetching todos:", error));
-    }, []);
 
+export const ShoppingList = () => {
+  const navigate = useNavigate();
+  const items = useSelector((state:RootState)=>state.shopping.items)
+  const userName = useSelector((state:RootState)=>state.user.name)
 
   return (
     <div className="p-6 min-h-screen min-w-screen bg-gray-100 flex flex-col items-center text-black">
@@ -33,20 +17,27 @@ import { useEffect, useState } from "react";
         <p className="text-gray-500">Nothing to buy yet!</p>
       ) : (
         <ul className="space-y-2 w-full max-w-md">
-        {todos.map((todo) => (
-          <li key={todo.id} className="p-2 bg-white rounded shadow">
-            <span>{todo.title}</span>
-          </li>
-        ))}
-      </ul>
-
+          {items.map((item, id) => (
+            <li
+              key={id}
+              className="flex items-center justify-between p-2 bg-white text-black rounded shadow"
+            >
+              <span
+              >
+                {item}
+              </span>
+            </li>
+          ))}
+        </ul>
       )}
       <button
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        className="mt-4 px-4 py-2 bg-blue-500 text-black rounded hover:bg-blue-600"
         onClick={() => navigate("/add-item")}
       >
         Add Item
       </button>
+
+      <TodoList/>
     </div>
   );
 };
